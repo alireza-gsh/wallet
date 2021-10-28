511ae9aeb3fb335d25e1f8e54f99cb1682bd1830
ghassmo <ghxxm@protonmail.com>
2021-10-28 14:35:38 +0300

    move const functions to the backend header

--- include/backend.h
+++ include/backend.h
@@ -25,13 +25,40 @@ class Backend : public QObject {
 public:
 	Backend(rpc::RpcClient* rpc_client);
 	~Backend();
-	QVariant get_tokens_info_list();
-	QVariantList get_own_tokens();
-	QVariantList get_networks();
-	QVariantList get_supported_deposit_tokens();
-	QVariantList get_supported_withdraw_tokens();
-	QString get_address();
-	QString get_notification();
+
+	QVariant get_tokens_info_list() const
+	{
+		return QVariant::fromValue(tokens_info_list);
+	}
+
+	QVariantList get_own_tokens() const { return own_tokens; }
+
+	QVariantList get_networks() const { return networks; }
+
+	QVariantList get_supported_deposit_tokens() const
+	{
+		QVariantList tokens = supported_deposit_tokens[selected_network];
+		return tokens;
+	}
+
+	QVariantList get_supported_withdraw_tokens() const
+	{
+		QVariantList tokens {};
+
+		for (auto token : tokens_info_list) {
+			if (token->get_network().toLower() == selected_network.toLower()) {
+				tokens.append(token->get_name());
+			}
+		}
+
+		return tokens;
+	}
+
+	QString get_address() const { return wallet_address; }
+
+	QString get_cashier_address() const { return cashier_address; };
+
+	QString get_notification() const { return notification; }
 
 public slots:
 	void update();
@@ -53,6 +80,7 @@ private:
 	rpc::RpcClient* rpc_client;
 	QString selected_network;
 	QString wallet_address;
+	QString cashier_address;
 	QString notification;
 
 	bool handle_error(boost::json::object& obj);
